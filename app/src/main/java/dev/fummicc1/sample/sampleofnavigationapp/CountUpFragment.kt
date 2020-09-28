5859d847cc955d1a581856624d5355bcaf1f2a62
fummicc1 <fumiya.tennis1@gmail.com>
2020-09-28 14:44:55 +0900

    feat: counting game starts on creating CountUpFragment screen.

--- app/src/main/java/dev/fummicc1/sample/sampleofnavigationapp/CountUpFragment.kt
+++ app/src/main/java/dev/fummicc1/sample/sampleofnavigationapp/CountUpFragment.kt
@@ -1,18 +1,61 @@
 package dev.fummicc1.sample.sampleofnavigationapp
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.UiThread
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import dev.fummicc1.sample.sampleofnavigationapp.data.CountRecord
+import dev.fummicc1.sample.sampleofnavigationapp.databinding.FragmentCountUpBinding
+import java.time.Duration
+import java.util.*
 
 class CountUpFragment : Fragment() {
+
+    private var countRecord: CountRecord = CountRecord()
+    private val timer: Timer = Timer()
+    private lateinit var handler: Handler
+    private lateinit var binding: FragmentCountUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_count_up, container, false)
+        binding = DataBindingUtil.inflate<FragmentCountUpBinding>(
+            inflater,
+            R.layout.fragment_count_up,
+            container,
+            false
+        )
+        binding.countRecord = countRecord
+        // TODO: fix deprecated Handler constructor.
+        // TODO: use coroutine instead of Handler.
+        handler = Handler()
+        startTimer(handler)
+        return binding.root
+    }
+
+    private fun startTimer(handler: Handler) {
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                handler.post {
+                    if (countRecord.time == 0) {
+                        Toast.makeText(context, getString(R.string.time_up), Toast.LENGTH_SHORT).show()
+                        timer.cancel()
+                        // need to specify function to return.
+                        return@post
+                    }
+                    countRecord.time -= 1
+                    binding.invalidateAll()
+                }
+            }
+        }, 0, 1000)
     }
 }
---
+++ app/src/main/java/dev/fummicc1/sample/sampleofnavigationapp/data/CountRecord.kt
@@ -0,0 +1,5 @@
+package dev.fummicc1.sample.sampleofnavigationapp.data
+
+import java.util.*
+
+data class CountRecord(var amount: Int = 0, var time: Int = 10, var date: Date = Date())
